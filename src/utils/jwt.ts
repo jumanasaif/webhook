6375b916a6c5d94546eed9ad474eb552b9fc22ac
Jumana Saif <jumanasaif2003@gmail.com>
2026-03-24 05:45:30 +0200

    edit jwt utils

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { env } from "../config/env.js";
 
 export function signToken(payload: any) {
-  return jwt.sign(payload, env.JWT_SECRET!,{ expiresIn: "1h" });
+  return jwt.sign(payload, env.JWT_SECRET!,{ expiresIn: "7d" });
 }
 
 export function verifyToken(token: string) {
